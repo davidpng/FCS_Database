140e8bfa9fca33088bddd6964aaa383a318918c7
davidpng <david.p.ng@gmail.com>
2014-10-21 09:14:44 -0700

    Added instruction on how to use header files

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ Created on Mon Oct 20 13:56:55 2014
 from HEADER_Find_FCS_Files import Find_Clinical_FCS_Files
 from HEADER_loadFCS import loadFCS
 
+Dir = "/home/ngdavid/Desktop/Ubuntu_Dropbox/Myeloid_Data/Myeloid"
+
+Finder = Find_Clinical_FCS_Files(Dir)
+
+FCS_metadata = []
+for f in Finder.filenames:
+    FCS_metadata.append( loadFCS(f) )
+    
