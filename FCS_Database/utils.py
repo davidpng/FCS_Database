5db39446f2221ddb0ce89899c3072ed2c782a656
hermands <hermands@uw.edu>
2014-11-02 12:34:26 -0800

    Move to package format

--- FCS_Database/utils.py
+++ FCS_Database/utils.py
@@ -81,30 +81,6 @@ def opener(pth, mode='r', bufsize=-1):
     return Opener(mode, bufsize)(pth)
 
 
-class GetVersion(object):
-    """ Identify the version number of this code for traceability """
-
-    def __init__(self):
-        # If git repository
-        self.version = self.load_git_version()
-
-    def load_git_version(self):
-        """ Get version of current git repository """
-
-        p = Popen(['echo $(git rev-parse --show-toplevel)'], stderr=PIPE, stdout=PIPE, shell=True)
-        repos_directory, err = p.communicate()
-
-        if err:
-            raise 'Cannot locate git repository directory'
-
-        repo = Repo(repos_directory.strip())
-        repo.config_reader()  # Set as read-only access
-        if repo.bare is True:
-            raise "Repository %s is bare!" % repos_directory
-
-        return repo.head.commit.hexsha[0:7]
-
-
 class Vividict(dict):
     def __missing__(self, key):
         value = self[key] = type(self)()
--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 FCS_Database
 ============
 
-Program to scrape an FCS directory of metadata
+Program to analyze flow cytometry .fcs files
 
 
 DEV setup
@@ -13,14 +13,17 @@ Load HP database meta information (example)
 FCS_Database-env/bin/activate
 
 # Make FCS db
-./FCS_Database/load_FCSdb.py --dir /home/local/AMC/ngdavid/clinical_cHL_cases --rebuilddb
+./flowanal.py make_FCSmeta_db --dir /home/local/AMC/ngdavid/clinical_cHL_cases/
 
 # Export Tube types data
-./FCS_Database/tube_types.py -export -file data/tube_types.tmp
+./flowanal.py tube_types -export
 
 # Import Tube types data
-./FCS_Database/tube_types.py -load -file data/tube_types.csv
+<edit> db/tube_types.tmp
+./flowanal.py tube_types -load
 
 # Query FCS db for Hodgkins tubes
-./FCS_Database/query_db.py -tubes Hodgkins -exporttype dict_dict
+./flowanal.py query_db --tubes Hodgkins --exporttype df --outfile db/tubetypes.tmp
 
+# Update package version
+python setup.py -h
