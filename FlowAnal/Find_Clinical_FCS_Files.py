2ec867bbf6d885f62fbc5ab0d7c2300edb03f4c9
davidpng <david.p.ng@gmail.com>
2014-11-10 10:13:48 -0800

    Updated Find_Clinical_FCS_Files to write found filelist to a file

--- FlowAnal/Find_Clinical_FCS_Files.py
+++ FlowAnal/Find_Clinical_FCS_Files.py
@@ -13,10 +13,11 @@ class Find_Clinical_FCS_Files(object):
     """
     Finds all FCS files matching a pattern NN-NNNNN in a given directory
     """
-    def __init__(self, directory):
+    def __init__(self, directory,**kwargs):
         self.directory = directory
         self.filenames = self.__find_files()
-
+        if "Filelist_Path" in kwargs:
+            self.write_found_files(kwargs['Filelist_Path'])
     def __find_files(self):
         filenames = [os.path.join(dirpath, f)
                      for dirpath, dirnames, files in os.walk(self.directory)
@@ -37,6 +38,17 @@ class Find_Clinical_FCS_Files(object):
             self.make_dict = True
         return folder_name
 
+    def write_found_files(self,Filelist_Path):
+        """
+        This will write the found filenames to a text file
+        """
+        dir_FoundFiles = os.path.join(os.getcwd(),Filelist_Path)
+        
+        with open(dir_FoundFiles,'w+') as fo:
+            for f in self.filenames:
+                fo.write(f+'\n')
+        fo.close()
+        print("FCS files found and saved to {}".format(dir_FoundFiles))
 
 if __name__ == '__main__':
     Dir = "/home/ngdavid/Desktop/Ubuntu_Dropbox/Myeloid_Data/Myeloid"
--- FlowAnal/subcommands/make_FCSmeta_db.py
+++ FlowAnal/subcommands/make_FCSmeta_db.py
@@ -4,7 +4,7 @@
 
 """
 import logging
-
+import os
 from FlowAnal.Find_Clinical_FCS_Files import Find_Clinical_FCS_Files
 from FlowAnal.FCS import FCS
 from FlowAnal.database.FCS_database import FCSdatabase
@@ -18,20 +18,22 @@ def build_parser(parser):
     parser.add_argument('-db', '--db', help='Output sqlite3 db for Flow meta data \
     [default: db/fcs.db]',
                         default="db/fcs.db", type=str)
-
+    parser.add_argument('-fl', '--fl',help='Output filelist of found FCS files\
+    [default: db/FoundFile.txt]', default='db/FoundFile.txt', type=str)
+    
 
 def action(args):
-        # Collect files/dirs
-        Finder = Find_Clinical_FCS_Files(args.dir)
+    # Collect files/dirs
+    Finder = Find_Clinical_FCS_Files(args.dir,Filelist_Path=args.fl)
 
-        # Connect to database (and rebuild)
-        db = FCSdatabase(db=args.db, rebuild=True)
-        print "Building database %s" % args.db
+    # Connect to database (and rebuild)
+    db = FCSdatabase(db=args.db, rebuild=True)
+    print "Building database %s" % args.db
 
-        # Process files/dirs
-        for f in Finder.filenames:
-            fFCS = FCS(filepath=f)
-            fFCS.meta_to_db(db=db, dir=args.dir, add_lists=True)
+    # Process files/dirs
+    for f in Finder.filenames:
+        fFCS = FCS(filepath=f)
+        fFCS.meta_to_db(db=db, dir=args.dir, add_lists=True)
 
 
 
