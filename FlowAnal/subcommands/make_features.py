2cb75925a82148c01f5249b6511ba163d02b5f78
davidpng <david.p.ng@gmail.com>
2015-01-06 14:06:03 -0800

    updated make_features

--- FlowAnal/subcommands/make_features.py
+++ FlowAnal/subcommands/make_features.py
@@ -99,6 +99,6 @@ def action(args):
                 except ValueError, e:
                     print "Skipping feature extraction for case: {} because of\
                            ValueError {}".format(case, e)
-                    feature_failed_CTIx.append = [case, case_tube_idx]
+                    feature_failed_CTIx.append([case, case_tube_idx])
     # push feature_failed_CTIx to database
     log.info("features failed: {}".format(feature_failed_CTIx))
