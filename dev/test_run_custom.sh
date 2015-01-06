14fd193326a981db17b2f544fbe70fd50334a4db
davidpng <david.p.ng@gmail.com>
2015-01-06 10:40:01 -0800

    added commented ideas about query schema to test_run_custom.sh

--- dev/test_run_custom.sh
+++ dev/test_run_custom.sh
@@ -24,9 +24,20 @@ python $FLOWANAL/setup.py -h >> /dev/null
 
 echo -e "\n################# Add custom data #################"
 $FLOWANAL/flowanal.py -v add_CustomCaseData_db $3 -db $2 -outdb $dbfile
+# outdb should only include cases in the metadb and the custom cases
 
 echo -e "\n################# Make features from data #########"
 $FLOWANAL/flowanal.py -v make_features $1 -db $dbfile -hdf5 $hdf5file
+# if feature_extraction fails
+# 1. will not include in HDF5 
+# 2. added case (via case_tube_idx) to exclude list and flag reason why
+# 3. Remove case from dbfile
+
+echo -e "\n################# pick correct case_tube_idx #########"
+#this will be a dbfile search that deletes a subset of case_tube_idx that are repeated
+#due to repeat processes (pick most recent rpt file)
+
 
 echo -e "\n################# Merge features #########"
 #$FLOWANAL/flowanal.py -v run_ML_on_TubeCases -db $dbfile -hdf5 $hdf5file
+# build checks to make sure HDF5 has case_tube_idx that is being query.
